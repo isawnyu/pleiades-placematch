47213a4b400dcfcac32fe4b46bb7e6f957ebdc7a
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2010-03-19 11:28:27 +0000

    Improved search form with overview and links to JSON, KML.

--- pleiades/placematch/matching.py
+++ pleiades/placematch/matching.py
@@ -3,12 +3,13 @@ from StringIO import StringIO
 import urllib
 import urllib2
 
-from plone.app.z3cform.layout import wrap_form
+from plone.app.z3cform.layout import FormWrapper #wrap_form
 from plone.memoize.instance import memoize
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from z3c.form import form, field, button
+from z3c.form.i18n import MessageFactory as _
 from zope import schema
 from zope.interface import Interface, Invalid, invariant
 
@@ -24,10 +25,13 @@ class GeoNamesSearch(object):
         return 'http://sws.geonames.org/%s/' % item['geonameId']
     def __call__(self, mn, co=None):
         """Returns Python data loaded from GeoNames JSON"""
-        # return [{'name': 'Foo'}]
         if not mn:
             return []
-        q = dict(name=mn.encode('utf-8'), type='json', featureClass='P')
+        try:
+            name = mn.encode('utf-8')
+        except UnicodeDecodeError:
+            name = mn
+        q = dict(name=name, type='json', featureClass='P')
         if co is not None:
             q.update(country=co)
         u = urllib2.urlopen('http://ws.geonames.org/search', urllib.urlencode(q))
@@ -57,7 +61,7 @@ def to_dict(metadata):
     except ValueError:
         bbox = None
         center = None
-    return dict(name=metadata.Title, 
+    return dict(name=unicode(metadata.Title, 'utf-8'), 
                 uid=metadata.UID, 
                 id=metadata.getId, 
                 placetype=metadata.getFeatureType, 
@@ -96,15 +100,21 @@ class PlaceMatcher(BrowserView):
 
     def update(self):
         self.key = '0'
-        self.mn = self.request.form.get('mn', None)
+        self.mn = unicode(self.request.form.get('mn', ''), 'utf-8') or None
         self.co = self.request.form.get('co', None)
-        self.an = self.request.form.get('an', None)
+        self.an = unicode(self.request.form.get('an', ''), 'utf-8') or None
         self.format =  self.request.form.get('f', '')
 
     @property
     @memoize
     def alternate_link(self):
-        return '%s/@@place-match-form' % self.context.absolute_url()
+        form_params = {'form.widgets.mn': self.mn.encode('utf-8') or '',
+                       'form.widgets.co': self.co or '',
+                       'form.widgets.an': self.an.encode('utf-8') or ''}
+        return '%s/@@place-match-form?%s' % (
+            self.context.absolute_url(),
+            urllib.urlencode(form_params)
+            )
 
     def matchsets(self):
         catalog = getToolByName(self.context, 'portal_catalog')
@@ -155,29 +165,49 @@ class IPlaceMatch(Interface):
     an = schema.TextLine(title=u'Ancient name', 
         description=u'Transliterated ancient place name', required=False)
 
-    f = schema.Choice(title=u'Results format', 
-                      values=('KML', 'JSON', 'Text'), 
-                      required=True)
-
 
 class Form(form.Form):
+    
     fields = field.Fields(IPlaceMatch)
+    
     ignoreContext = True # don't use context to get widget data
+    ignoreRequest = False
+    
     label = u'Search across modern and ancient names for possible matches'
+    _results = None
+    _data = None
 
-    def update(self):
-        super(Form, self).update()
-        # Fix widget names
-        for key, value in self.widgets.items():
-            value.name = key
+    @button.buttonAndHandler(_('Search'), name='search')
+    def handleSearch(self, action):
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
+        self._data = data
+        catalog = getToolByName(self.context, 'portal_catalog')
+        s = MatchSet('0', data.get('mn'), data.get('co'), data.get('an'), 
+                     catalog, self.request)
+        self._results = dict(modern=s.modern_places(), 
+                             ancient=s.ancient_places())
 
-    @button.buttonAndHandler(u'Apply')
-    def handleApply(self, action):
-        query = self.request.form.copy()
-        query['f'] = query['f'][0].lower()
-        self.request.response.redirect(
-            '@@place-match?%s' % urllib.urlencode(query))
+    @button.buttonAndHandler(_('Clear'), name='clear')
+    def handleClear(self, action):
+        self._data = None
+        self._results = None
 
-PlaceMatcherForm = wrap_form(Form)
 
+class PlaceMatcherForm(FormWrapper):
+    form = Form
+    index=ViewPageTemplateFile('place_match_form.pt')
+   
+    @property
+    def results(self):
+        return self.form_instance._results
+
+    def detail_url_base(self):
+        query = {}
+        for k, v in self.form_instance._data.items():
+            if v is not None:
+                query[k] = v.encode('utf-8')
+        return '@@place-match?%s' % urllib.urlencode(query)
 
